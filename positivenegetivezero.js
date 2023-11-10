e840c38350f99e037c5d578af4a328cf0e1d5187
amjad <amjad94rangrej@gmail.com>
2023-11-10 19:39:10 +0530

    positivenumber

---
+++ positivenegetivezero.js
@@ -0,0 +1,14 @@
+// cheack if number positive negative or zero
+
+let n=-10
+if(n>0){
+    console.log("number is positive")
+}
+else if(n==0){
+    console.log("number is zero")
+
+}
+else{
+    console.log("number is negative")
+}
+ 
